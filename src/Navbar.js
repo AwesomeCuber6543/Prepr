9e9b0212a469137fa1323a953fdc4326ccd47b57
Yahia Salman <yahia.salman.04@gmail.com>
2023-10-08 04:56:44 -0400

    final frontend commit

--- src/Navbar.js
+++ src/Navbar.js
@@ -7,9 +7,9 @@ const Navbar = () => {
       <div><img src = {img1} alt = "" style={{width: '100px', height: '100px'}}/></div>
       <h1 style = {{textAlign: 'center'}}>Mastering Interviews with AI!</h1>
       <div className="links">
-        <a href="/create" style ={{
+        {/* <a href="/create" style ={{
             color: 'white', backgroundColor: '#f1356d', borderRadius: '8px', textAlign: 'left'
-         }}>History</a>
+         }}>History</a> */}
       </div>
     </nav>
   );
